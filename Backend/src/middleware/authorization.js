210cd085b5a569ad217d5840156d4896362e9a5c
Kushagra Bajpai <bajpaikushagra610@gmail.com>
2024-07-05 20:39:19 +0530

    Frontend

---
+++ Backend/src/middleware/authorization.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const authorization = (req, res, next) => {
+  const token = req.headers?.authorization;
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+  try {
+    const isValid = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+
+    if (isValid) {
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json({ token: true });
+  }
+};
+module.exports = authorization;
