c768e32fe55bfbe8526b2c511a8e0b9257fc870d
Kushagra Bajpai <73277903+Kushagra-tech@users.noreply.github.com>
2024-07-06 16:48:02 +0530

    Delete Backend directory

--- Backend/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const serverSetup = require("./server");
-const dbConnection = require("./src/config/db");
-const PORT = process.env.PORT;
-dbConnection()
-  .then(() => {
-    serverSetup().listen(PORT, () => console.log("Listening at :", PORT));
-  })
-  .catch((error) => console.log("Connection problem :", error.message));
--- Backend/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register, login, verify } = require("../controllers/user.controller");
-
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-router.post("/verify", verify);
-module.exports = router;
